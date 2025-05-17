401b6230d7c480f6f1447e70ed893e4b82c5d8f7
harfool <harfoolgujjar63@gmail.com>
2025-05-18 01:14:42 +0530

    Leetlab backend setup

---
+++ backend/src/index.js
@@ -0,0 +1,18 @@
+import express from "express"
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+
+
+const Port = process.env.PORT ||3000
+
+
+
+
+
+app.listen(Port , ()=>{
+    console.log(`server running on ${Port}`);
+    
+})
