a783831c50674da6afd73dd365a38fd0e8945a68
harfool <harfoolgujjar63@gmail.com>
2025-05-18 02:09:58 +0530

    auth route and controller

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,16 @@
 import express from "express"
 import dotenv from 'dotenv'
+import authRoutes from "./routes/auth.routes.js"
 
 dotenv.config()
 
 const app = express()
+app.use(express.json())
+app.use("/api/v1/auth", authRoutes)
+
+app.get('/', (req,res)=>{
+    res.send("welcome to leetlab ")
+})
 
 
 const Port = process.env.PORT ||3000
