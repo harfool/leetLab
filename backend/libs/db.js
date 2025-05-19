c6249fc215f4424fd306963be3e4e2d48e4c09da
harfool <harfoolgujjar63@gmail.com>
2025-05-19 12:27:52 +0530

    authentication contoroller
    login , resister , logout authentication contoroller

--- backend/libs/db.js
+++ backend/libs/db.js
@@ -1,9 +1,10 @@
-import {PrismaClient} from '../generated/prisma/index.js '
+//import {PrismaClient} from '../generated/prisma/index.js '
+import {PrismaClient} from '../src/generated/prisma/index.js'
 
 
-const globalForPrisma = global.this
+const globalForPrisma = globalThis;
 
 
-export const db = globalForPrisma.prisma || new PrismaClient
+export const db = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production")  globalForPrisma.prisma = db
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,14 @@
 import express from "express"
 import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
+
 import authRoutes from "./routes/auth.routes.js"
 
 dotenv.config()
 
 const app = express()
 app.use(express.json())
+app.use(cookieParser())
 app.use("/api/v1/auth", authRoutes)
 
 app.get('/', (req,res)=>{
