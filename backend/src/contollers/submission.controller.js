b62643989d4e7f7d6ecb6fa715a8f50492f0fe9e
harfool <harfoolgujjar63@gmail.com>
2025-05-27 17:06:19 +0530

    Update submission.controller.js

--- backend/src/contollers/submission.controller.js
+++ backend/src/contollers/submission.controller.js
@@ -17,17 +17,60 @@ try {
     })
 
 } catch (error) {
-    console.error("error" , error)
+    console.error("Error in get All Submission " , error)
     res.status(500).json({
         error : "Error while fetching submission"
     })
 }
 }
 
-export const getSubmissionsForProblem = async () =>{
-
+export const getSubmissionsForProblem = async (req , res ) =>{
+try {
+    
+        const userId = req.user.id
+        const problemId = req.params.problemId
+        const submissions = await db.submission.findMany({
+            where :{
+                userId : userId ,
+                problemId : problemId
+    
+            }
+        })
+    
+        res.status(200).json({
+           success : true ,
+            message : "Submission problem fetch successfully",
+            submissions
+        })
+    
+} catch (error) {
+    console.error("Error in get Submissions For Problem" ,error)
+    res.status(500).json({
+        error : " failed Submission problem fetch "
+    })
+}
 }
 
-export const getAllTheSubmissionForProblem = async () =>{
+export const getAllTheSubmissionForProblem = async (req , res ) =>{
+try {
+    const problemId = req.params.problemId
+    const submission = await db.submission.count({
+        where : {
+            problemId : problemId
+        }
 
+    })
+  
+    res.status(200).json({
+        success : true ,
+        message : " Fetch Submission For Problem is Successfully ",
+        submission
+    })
+
+} catch (error) {
+    console.error("error in get All The Submission For Problem" , error)
+    res.status(500).json({
+        error : "failed to get All The Submission For Problem"
+    })
+}
 }
