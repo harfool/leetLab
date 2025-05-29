d1b78384dd4a1ad68299ab97589cba36f6970ba4
harfool <harfoolgujjar63@gmail.com>
2025-05-29 11:03:59 +0530

    add store , axiosinstance and fix cors error

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 import express from "express"
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
-
+import cors from "cors"
 import authRoutes from "./routes/auth.routes.js"
 import problemRoutes from "./routes/problem.routes.js"
 import executionRoute from "./routes/executeCode.routes.js"
@@ -13,6 +13,13 @@ dotenv.config()
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
+app.use(
+    cors({
+        origin : "http://localhost:5173",
+        credentials : true
+    })
+)
+
 app.use("/api/v1/auth", authRoutes)
 app.use("/api/v1/problems", problemRoutes)
 app.use("/api/v1/execute-code", executionRoute)
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,31 @@
-import { useState } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
+import { Toaster } from 'react-hot-toast';
+import { useAuthStore } from './store/useAuthStore';
+import { useEffect } from 'react';
+import { Loader } from 'lucide-react';
 
 const App = () => {
-  const [authUser, setAuthUser] = useState(null); 
+
+   const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();  
+  }, [checkAuth]);
+
+  if (isCheckingAuth && !authUser) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
+  }
+
   return (
     <div className='flex flex-col items-center justify-start'>
+      <Toaster/>
       <Routes>
         <Route path='/' element={authUser ? <HomePage /> : <Navigate to='/login' />} />
         <Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to='/' />} />
