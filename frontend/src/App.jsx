5b39eef01ccfd4167551424b972b4fa30a7ecb39
harfool <harfoolgujjar63@gmail.com>
2025-05-29 13:52:41 +0530

    Addproblem problemschema

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,8 @@ import { useAuthStore } from './store/useAuthStore';
 import { useEffect } from 'react';
 import { Loader } from 'lucide-react';
 import Layout from './layout/Layout';
+import AdminRoute from './components/AdminRoute';
+import AddProblem from './pages/AddProblem';
 
 const App = () => {
 
@@ -41,7 +43,17 @@ const App = () => {
         <Route 
         path='/signup' 
         element={!authUser ? <SignUpPage /> : <Navigate to='/' />} />
+
+       <Route element={<AdminRoute />}>
+          <Route
+            path="/add-problem"
+            element={authUser ? <AddProblem /> : <Navigate to="/" />}
+          />
+        </Route>
+
       </Routes>
+
+      
     </div>
   );
 };
