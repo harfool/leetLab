d7d092a88307096791427c81ca2749b13f04d5d9
harfool <harfoolgujjar63@gmail.com>
2025-05-30 15:53:38 +0530

    problem table

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,23 @@
 
 import { Loader } from "lucide-react";
+import { useProblemStore } from "../store/useProblemStore";
+import { useEffect } from "react";
+import ProblemsTable from "../components/ProblemsTable";
 
 const HomePage = () => {
+const {getAllProblems , problems , isProblemsLoading } = useProblemStore()
 
+  useEffect(()=>{
+  getAllProblems()
+}, [getAllProblems])
 
+if (isProblemsLoading) {
+  return(
+    <div className="flex items-center justify-center h-screen" >
+      <Loader className="size-10 animate-spin" />
+    </div>
+  )
+}
   return (
     <div className="min-h-screen flex flex-col items-center mt-14 px-4">
       <div className="absolute top-16 left-0 w-1/3 h-1/3 bg-primary opacity-30 blur-3xl rounded-md bottom-9"></div>
@@ -16,8 +30,14 @@ const HomePage = () => {
         interviews and helps you to improve your coding skills by solving coding
         problems
       </p>
-
-      
+    {
+      problems.length > 0 ? <ProblemsTable problems ={problems}/> : (
+        <p className="mt-10 text-center text-lg font-semibold text-gray-500 dark:text-gray-400 z-10 border border-primary px-4 py-2 rounded-md border-dashed">
+          No problems found
+        </p>
+      )
+    }
+       
     </div>
   );
 };
